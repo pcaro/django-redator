78b801a0a1fb07d53b87fdd037d7e532877066f4
Guilherme Gondim <semente@taurinus.org>
2012-04-11 16:59:43 -0300

    Minor change.

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup, find_packages
 
-setup(name="django-redactor",
-           version="0.2",
-           description="Django application for http://imperavi.ru/redactor/",
-           author="Vladimir Sidorenko",
-           author_email="yoyavova@gmail.com",
-           packages=find_packages(),
-           include_package_data=True,
+setup(
+    name="django-redactor",
+    version="0.3",
+    description="Django application for http://imperavi.ru/redactor/",
+    author="Vladimir Sidorenko",
+    author_email="yoyavova@gmail.com",
+    packages=find_packages(),
+    include_package_data=True,
 )
