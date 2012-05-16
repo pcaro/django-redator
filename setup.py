99fb48d643d99aadc4acb91c3fbe8327e8839948
Guilherme Gondim <semente@taurinus.org>
2012-05-15 21:25:05 -0300

    release 0.3.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-redactor",
-    version="0.3",
+    version="0.3.1",
     description="Django application for http://imperavi.ru/redactor/",
     author="Vladimir Sidorenko",
     author_email="yoyavova@gmail.com",
