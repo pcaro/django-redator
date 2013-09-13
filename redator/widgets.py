c8da3e43f36d37e754e8c047328df3ee037dffe6
Guilherme Gondim <semente@taurinus.org>
2013-09-13 16:26:44 -0300

    Fix CSS URL and URL names

--- redator/widgets.py
+++ redator/widgets.py
@@ -9,13 +9,13 @@ from django.utils.safestring import mark_safe
 class RedactorEditor(widgets.Textarea):
     init_js = (
         '<script type="text/javascript">'
-        'jQuery(document).ready(function(){$("#%s").redactor(%s);});'
+        'jQuery(document).ready(function(){jQuery("#%s").redactor(%s);});'
         '</script>'
     )
 
     class Media:
         js = ('redactor/redactor.min.js',)
-        css = {'all': ('redactor/css/redactor.css',)}
+        css = {'all': ('redactor/redactor.css',)}
 
     def __init__(self, *args, **kwargs):
         super(RedactorEditor, self).__init__(*args, **kwargs)
@@ -23,11 +23,17 @@ class RedactorEditor(widgets.Textarea):
         self.widget_options = kwargs.pop('redactor_options', {})
 
     def get_options(self):
-        options = etattr(settings, 'REDATOR_REDACTOR_OPTIONS', {})
+        options = getattr(settings, 'REDATOR_REDACTOR_OPTIONS', {})
         options.update(self.widget_options)
         options.update({
-            'imageUpload': reverse('redactor_upload_image', kwargs={'upload_to': self.upload_to}),
-            'fileUpload': reverse('redactor_upload_file', kwargs={'upload_to': self.upload_to})
+            'imageUpload': reverse(
+                'redator:upload-image',
+                kwargs={'upload_to': self.upload_to}
+            ),
+            'fileUpload': reverse(
+                'redator:upload-file',
+                kwargs={'upload_to': self.upload_to}
+            )
         })
         return json.dumps(options)
 
