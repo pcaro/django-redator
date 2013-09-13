1c7430e1394e2e4dd22992c2f84173fe47c37e53
Guilherme Gondim <semente@taurinus.org>
2013-09-13 11:45:49 -0300

    Improve setup

--- setup.py
+++ setup.py
@@ -1,16 +1,41 @@
 #/usr/bin/env python
 
+import codecs
+import os
+import sys
+
 from setuptools import setup, find_packages
 
 
+if 'publish' in sys.argv:
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+read = lambda filepath: codecs.open(filepath, 'r', 'utf-8').read()
+
+
+# Dynamically calculate the version based on redator.VERSION.
+version = __import__('redator').get_version()
+
 setup(
-    name="django-redactor",
-    version="0.4",
-    description="Django application for http://imperavi.ru/redactor/",
-    author="Vladimir Sidorenko",
-    author_email="yoyavova@gmail.com",
+    name='django-redator',
+    version=version,
+    description=(
+        'Django Redator (sic) helps you integrate Redactor '
+        '<http://imperavi.com/redactor/> to your Django forms. Redactor '
+        'is a beautiful and easy-to-use WYSIWYG HTML editor.'
+    ),
+    long_description=read(os.path.join(os.path.dirname(__file__), 'README.rst')),
+    keywords = 'django app wysiwyg editor redactor',
+    author='Vladimir Sidorenko, Guilherme Gondim',
+    author_email='semente+django-redator@taurinus.org',
+    maintainer='Guilherme Gondim',
+    maintainer_email='semente+django-redator@taurinus.org',
     license='BSD License',
+    url='https://bitbucket.org/semente/django-redator/',
+    download_url='https://bitbucket.org/semente/django-redator/downloads/',
     packages=find_packages(),
+    zip_safe=False,
     include_package_data=True,
     classifiers=[
         'Development Status :: 5 - Production/Stable',
@@ -22,4 +47,5 @@ setup(
         'Programming Language :: Python',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
+    #install_requires=[],
 )
