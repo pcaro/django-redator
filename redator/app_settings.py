037d6f60788de237e384aff23c3cbd554521cddd
Guilherme Gondim <semente@taurinus.org>
2013-09-14 00:51:31 -0300

    Add cleanup option as True

--- redator/app_settings.py
+++ redator/app_settings.py
@@ -3,6 +3,7 @@ from django.conf import settings
 
 UPLOAD_TO = getattr(settings, 'REDATOR_UPLOAD_TO', 'redator/%Y-%m/')
 REDACTOR_OPTIONS = getattr(settings, 'REDATOR_REDACTOR_OPTIONS', {
+    'autoresize': False,
+    'cleanup': True,
     'wym': True,
-    'autoresize': False
 })
