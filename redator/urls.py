42062023a44de37f4220f72631c70b506e90655b
Guilherme Gondim <semente@taurinus.org>
2013-09-14 00:39:29 -0300

    Minor change

--- redator/urls.py
+++ redator/urls.py
@@ -3,14 +3,9 @@ from django.conf.urls import url, patterns
 from .forms import FileForm, ImageForm
 
 
-urlpatterns = patterns('redator.views',
-    url('^upload/image/(?P<upload_to>.*)', 'upload', {
-        'form_class': ImageForm,
-    }, name='upload-image'),
-
-    url('^upload/file/(?P<upload_to>.*)', 'upload', {
-        'form_class': FileForm,
-    }, name='upload-file'),
-
+urlpatterns = patterns(
+    'redator.views',
+    url('^upload/image/(?P<upload_to>.*)', 'upload', {'form_class': ImageForm}, name='upload-image'),
+    url('^upload/file/(?P<upload_to>.*)', 'upload', {'form_class': FileForm}, name='upload-file'),
     url('^images.json', 'images_json', name='images-json'),
 )
