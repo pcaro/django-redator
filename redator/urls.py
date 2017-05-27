0b442e0ff56c017b749eb8fd19f24f732e985322
Pablo Caro Revuelta <correo@pablocaro.es>
2017-05-27 09:23:30 +0200

    Improve python support

--- redator/urls.py
+++ redator/urls.py
@@ -1,11 +1,10 @@
-from django.conf.urls import url, patterns
+from django.conf.urls import url
 
 from .forms import FileForm, ImageForm
 
 
-urlpatterns = patterns(
-    'redator.views',
-    url('^upload/image/', 'upload', {'form_class': ImageForm}, name='upload-image'),
-    url('^upload/file/', 'upload', {'form_class': FileForm}, name='upload-file'),
-    url('^images.json', 'images_json', name='images-json'),
-)
+urlpatterns = [
+    url('^upload/image/', 'redator.views.upload', {'form_class': ImageForm}, name='upload-image'),
+    url('^upload/file/', 'redator.views.upload', {'form_class': FileForm}, name='upload-file'),
+    url('^images.json', 'redator.views.images_json', name='images-json'),
+]
