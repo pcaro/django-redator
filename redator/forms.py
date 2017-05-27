62bea32b7e028984164773d4d7a7972946f992b1
Pablo Caro Revuelta <correo@pablocaro.es>
2017-05-27 09:31:23 +0200

    Django version improvement

--- redator/forms.py
+++ redator/forms.py
@@ -6,8 +6,10 @@ from . import models
 class FileForm(forms.ModelForm):
     class Meta:
         model = models.File
+        fields = '__all__'
 
 
 class ImageForm(forms.ModelForm):
     class Meta:
         model = models.Image
+        fields = '__all__'
